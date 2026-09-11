86c8ffdb52c54a5e490b57b050eb56f5a29e8677
Nguyễn Đức Tuệ <ductuethtth@gmail.com>
2026-09-11 09:56:56 +0700

    Implement systemd timer and crontab scheduling
    Add support for systemd timers in addition to crontab for scheduling the DNS re-resolution script.

--- add-wireguard-cron.sh
+++ add-wireguard-cron.sh
@@ -2,6 +2,19 @@
 
 RERESOLVE_DNS_SCRIPT_PATH="/usr/share/doc/wireguard-tools/examples/reresolve-dns/reresolve-dns.sh"
 RERUN_IN_MINUTE=1
+SCHEDULE_SERVICE=
+
+if [ -d /run/systemd/system ]; then 
+    echo "Using systemd."
+    SCHEDULE_SERVICE=systemd
+elif crontab -l >/dev/null 2>&1; then
+    echo "Using crontab."
+    SCHEDULE_SERVICE=crontab
+else
+    echo "Cannot find systemd or crontab in this machine."
+    echo "Please install either of them then re-run this scripts."
+    exit 1
+fi
 
 if [ "$EUID" -ne 0 ]; then
     printf "Error: Please run with sudo or as root.\n"
@@ -29,9 +42,44 @@ if [ ! -f $INTERFACE_CONFIG_PATH ]; then
     exit 1
 fi
 
-line="*/$RERUN_IN_MINUTE * * * * $RERESOLVE_DNS_SCRIPT_PATH  $INTERFACE_CONFIG_PATH"
-(sudo crontab -l; echo "$line" ) | sudo crontab -
+if [[ $SCHEDULE_SERVICE == "systemd" ]]; then
+    cat >/etc/systemd/system/${INTERFACE}_dns.service <<EOF
+[Unit]
+Description=Cron job to re-resolve the wireguard dns ${INTERFACE} interface
+Wants=network-online.target
+After=network-online.target
 
-sudo systemctl restart cron
+[Service]
+Type=oneshot
+ExecStart=$RERESOLVE_DNS_SCRIPT_PATH $INTERFACE_CONFIG_PATH
+EOF
 
-printf "Create cron job for $INTERFACE succesfully!\n"
+    cat >/etc/systemd/system/${INTERFACE}_dns.timer <<EOF
+[Unit]
+Description=${INTERFACE} interface timer
+Requires=${INTERFACE}_dns.service
+
+[Timer]
+Unit=${INTERFACE}_dns.service
+OnCalendar=*:0/$RERUN_IN_MINUTE
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+    sudo systemctl daemon-reload
+
+    sudo systemctl enable ${INTERFACE}_dns.timer
+    sudo systemctl start ${INTERFACE}_dns.timer
+    printf "Create cron job for $INTERFACE succesfully!\n"
+
+elif [[ $SCHEDULE_SERVICE == "crontab" ]]; then
+    line="*/$RERUN_IN_MINUTE * * * * $RERESOLVE_DNS_SCRIPT_PATH $INTERFACE_CONFIG_PATH"
+    (sudo crontab -l; echo "$line" ) | sudo crontab -
+
+    sudo systemctl restart cron
+
+    printf "Create cron job for $INTERFACE succesfully!\n"
+fi
+
